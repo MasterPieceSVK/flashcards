3380b422eba4198b1caa8966c46f26a3cb0797d4
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-03-09 22:09:01 +0100

    new features

--- info.js
+++ info.js
@@ -1,4 +1,4 @@
-// const info = "https://flashcards-backend-hl8y.onrender.com";
-const info = "http://localhost:5000";
+const info = "https://flashcards-backend-hl8y.onrender.com";
+// const info = "http://localhost:5000";
 
 export default info;
