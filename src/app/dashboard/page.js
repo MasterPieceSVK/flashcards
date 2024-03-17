a8a0d971eb153c3f90af44206d2994b8ba08f70b
MasterPieceSVK <112313437+MasterPieceSVK@users.noreply.github.com>
2024-03-17 14:44:06 +0100

    no logging of token

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -42,7 +42,6 @@ export default function Dashboard() {
   });
   useEffect(() => {
     const token = localStorage.getItem("token");
-    console.log(token);
     authMutation.mutate(token);
   }, []);
 
